53bf80e3a599aa9a44dcee65f4ae05b8764f5c37
Chris Schmidt <cmschmidt12345@gmail.com>
2023-02-04 17:01:09 -0500

    Working on Tree edge cases

--- Assets/Prefabs/Tree/TreeSpawn.cs
+++ Assets/Prefabs/Tree/TreeSpawn.cs
@@ -6,6 +6,10 @@ public class TreeSpawn : MonoBehaviour
 {
     public Transform respawnPoint;
 
+    [SerializeField] private GameObject body;
+
+    [SerializeField] private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +19,39 @@ public class TreeSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ((Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.W)))
+                {
+Debug.Log("Button Pressed");                  
+                }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Player")
         {
-        //To do: activate tree
-           collider.transform.position = respawnPoint.position;
+            if (body.activeSelf == true) 
+            {
+Debug.Log("already active");
+                
+            }
+            else 
+            {
+            body.SetActive(true);
+            animator.SetTrigger("Grow");
+            StartCoroutine(OnTreeGrown(collider.gameObject));
+            }
         }
+    }
+
+    public IEnumerator OnTreeGrown(GameObject player) 
+    {
+Debug.Log(animator.GetCurrentAnimatorClipInfo(0));
+        AnimatorClipInfo clipInfo = animator.GetCurrentAnimatorClipInfo(0)[0];
+        
+        while (clipInfo.clip.length > animator.playbackTime) {
+            yield return null;
+        }
+
+        player.transform.position = respawnPoint.position;
     }
 }
